771b7edd08174faccc379590bb529d6919064f95
Byron Chen <bchen9694@bths.edu>
2018-10-02 16:07:10 -0700

    Complete project's basic, still have to fix ones with multiple outputs.

--- src/BChenLib.java
+++ src/BChenLib.java
@@ -5,10 +5,48 @@ public class BChenLib {
         String y = date.substring(6,10);
         return (d+"-"+m+"-"+y);
     }
+//////////////////////good
     public static String cutOut(String mainString, String subString){
-        int start = mainString.indexOf(subString);
-        String part1= subString.substring(0, start);
-        String part2= subString.substring(start+subString.length(), mainString.length());
-        return (part1+part2);
+        String first = subString.substring(0,1);
+        int start = mainString.indexOf(first);
+        String part1= mainString.substring(0, start);
+        String part2= mainString.substring(start+subString.length(), mainString.length());
+        String answer = part1+part2;
+        return answer;
     }
+/////////////////////good
+    public static void fooBarBaz(int num1){
+        for(int i=1; i<num1+1; i++) {
+            if(i%5==0 && i%3==0){
+                System.out.println("baz");;
+                if(i%3==0){
+                    System.out.println("foo");
+                    if(i%5==0){
+                        System.out.println("bar");
+                    } else {
+                        System.out.println(i);
+                    }
+                }
+            }
+        }
+    }
+//CANT USE VOID
+    public static int sumUpToCombine(int number){
+        int answer = 0;
+        for(int i=0;i<number+1;i++){
+            answer += i;
+        }
+        return answer;
+    }
+///////////////////good
+    public static int primePrinter(int number){
+        for(int i=2; i<number; i++){
+            if(number%i!=0){
+                return(i);
+            }
+        }
+        return(69);
+    }
+//////////ask about if number = 2, make it so that it gives all the prime, not just first
+
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -2,5 +2,8 @@ public class TestFile {
     public static void main(String [] args)
     {
         System.out.println(BChenLib.dateStr("11/22/3333"));
+        System.out.println(BChenLib.cutOut("giantcatpoop","cat"));
+        System.out.println(BChenLib.sumUpToCombine(5));
+        System.out.println(BChenLib.primePrinter(2));
     }
 }
