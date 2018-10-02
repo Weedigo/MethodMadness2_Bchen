0ab95cdc3aedc38df4ca000db6d7378196514dec
BchenCommit1 <bchen9694@bths.edu>
2018-10-02 15:15:39 -0400

    added cutout

--- src/BChenLib.java
+++ src/BChenLib.java
@@ -5,4 +5,10 @@ public class BChenLib {
         String y = date.substring(6,10);
         return (d+"-"+m+"-"+y);
     }
+    public static String cutOut(String mainString, String subString){
+        int start = mainString.indexOf(subString);
+        String part1= subString.substring(0, start);
+        String part2= subString.substring(start+subString.length(), mainString.length());
+        return (part1+part2);
+    }
 }
