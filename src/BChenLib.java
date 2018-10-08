3665a1fe82374cf243788cb3ee248a2e6dcf45d8
Byron Chen <bchen9694@bths.edu>
2018-10-07 21:10:00 -0700

    SUNDAY

--- src/BChenLib.java
+++ src/BChenLib.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class BChenLib {
     public static String dateStr(String date){
         String m = date.substring(0,2);
@@ -36,15 +38,70 @@ public class BChenLib {
         return answer;
     }
 ///////////////////good
-    public static int primePrinter(int number){
-        for(int i=2; i<number; i++){
-            if(number%i!=0){
-                return(i);
+    public static void primePrinter(int number){
+        for (int i=2; i<number; i++){
+            int check = 0;
+            for (int k=2; k<i; k++){
+                if (i % k == 0) {
+                    check = 1;
+                }
+            }
+            if (check == 0) {
+                System.out.println(i);
             }
         }
-        return(69);
     }
 //////////ask about if number = 2, make it so that it gives all the prime, not just first
-    //My league elo problem
+    public static String quadSolver(double a, double b, double c){
+        double desc= Math.pow(b, 2)- 4*a*c;
+        if (desc >= 0) {
+            double ans1= (-b+ Math.sqrt(Math.pow(b, 2)- 4*a*c))/ (2*a);
+            double ans2= (-b- Math.sqrt(Math.pow(b, 2)- 4*a*c))/ (2*a);
+            return(String.valueOf(ans1) + "," + String.valueOf(ans2));
+        } else {
+            return("The root of this quadratic is imaginary, this is because it's descriminant is negative");
+        }
+    }
+
+    ////////////////////
+    public static int leastCommonMultiple(int num1, int num2, int num3){
+        int max1 =0; int max2 =0; int max3 =0; int zero = 0;
+        if(num2>num1 && num1>num3){
+            max1= num2;
+            max2= num1;
+            max3= num3;
+        }
+        if(num3>num1 && num1>num2){
+            max1= num3;
+            max2= num1;
+            max3= num2;
+        }
+        if(num1>num2 && num2>num3){
+            max1= num1;
+            max2= num2;
+            max3= num3;
+        }
+        if(num3>num2 && num2>num1){
+            max1= num3;
+            max2= num2;
+            max3= num1;
+        }
+        if(num1>num3 && num3>num2){
+            max1= num1;
+            max2= num3;
+            max3= num2;
+        }
+        if(num2>num3 && num3>num1){
+            max1= num2;
+            max2= num3;
+            max3= num1;
+        }
+        for(int i=0;i<max1;i++){
+            if((max1%i)==(max2%i)==(max3%i)==(zero))
+                return(i);
+        }
+        return(123);
+        answre is on stack OVERFLOWWW
+    }
 
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -4,7 +4,9 @@ public class TestFile {
         System.out.println(BChenLib.dateStr("11/22/3333"));
         System.out.println(BChenLib.cutOut("giantcatpoop","cat"));
         System.out.println(BChenLib.sumUpToCombine(5));
-        System.out.println(BChenLib.primePrinter(2));
-        BChenLib.fooBarBaz(100);
+        BChenLib.fooBarBaz(15);
+        BChenLib.primePrinter(7);
+        System.out.println(BChenLib.quadSolver(1,-6,187));
+        System.out.println(BChenLib.leastCommonMultiple(3,6,15));
     }
 }
